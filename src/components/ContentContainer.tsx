3a5e6504b101bdc0403e33e45cc058241b3044c6
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-03-16 17:28:57 -0300

    Adicionar função de Loading para esperar receber a requisição da api.

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -41,10 +41,12 @@ export function ContentContainer({ data }: IProps) {
       });
 
       setFilteredDigimons(filteredData);
+      setLoading(false);
       return;
     } else {
       if (level === "Todos") {
         setFilteredDigimons(digimons);
+        setLoading(false);
         return;
       }
 
@@ -55,6 +57,7 @@ export function ContentContainer({ data }: IProps) {
       });
 
       setFilteredDigimons(filteredData);
+      setLoading(false);
     }
   }, [level, name, digimons, showMore]);
 
@@ -62,93 +65,104 @@ export function ContentContainer({ data }: IProps) {
     <div className="w-full flex flex-col items-center justify-center gap-8">
       <SearchBar setName={setName} setLevel={setLevel} />
 
-      {filteredDigimons.length === 0 ? (
+      {loading ? (
         <>
-          <h1 className="text-3xl font-bold text-red-900 text-center">
-            Nenhum digimon com esse nome foi encontrado :(
-          </h1>
-
-          <div className="w-full h-96 flex items-center justify-center">
-            <Image
-              src={"/imgs/Culumon_triste.gif"}
-              alt="404"
-              height={300}
-              width={300}
-              className="w-auto h-auto"
-            />
-          </div>
+          <div
+            className="inline-block h-20 w-20 my-44 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+            role="status"
+          ></div>
         </>
       ) : (
         <>
-          <ul className="flex flex-wrap lg:grid lg:grid-cols-3 gap-5 mx-auto px-4 items-center justify-center ">
-            {filteredDigimons.map((digimon, index) => {
-              if (showMore) {
-                if (level === "Todos") {
-                  return (
-                    <DigimonCard
-                      key={digimon.name}
-                      name={digimon.name}
-                      imgUrl={digimon.img}
-                      level={digimon.level}
-                    />
-                  );
-                }
+          {filteredDigimons.length === 0 ? (
+            <>
+              <h1 className="text-3xl font-bold text-red-900 text-center">
+                Nenhum digimon com esse nome foi encontrado :(
+              </h1>
 
-                if (digimon.level === level) {
-                  return (
-                    <DigimonCard
-                      key={digimon.name}
-                      name={digimon.name}
-                      imgUrl={digimon.img}
-                      level={digimon.level}
-                    />
-                  );
-                }
-              } else {
-                if (index < 3) {
-                  if (level === "Todos") {
-                    return (
-                      <DigimonCard
-                        key={digimon.name}
-                        name={digimon.name}
-                        imgUrl={digimon.img}
-                        level={digimon.level}
-                      />
-                    );
-                  }
-
-                  if (digimon.level === level) {
-                    return (
-                      <>
+              <div className="w-full h-96 flex items-center justify-center">
+                <Image
+                  src={"/imgs/Culumon_triste.gif"}
+                  alt="404"
+                  height={300}
+                  width={300}
+                  className="w-auto h-auto"
+                />
+              </div>
+            </>
+          ) : (
+            <>
+              <ul className="flex flex-wrap lg:grid lg:grid-cols-3 gap-5 mx-auto px-4 items-center justify-center ">
+                {filteredDigimons.map((digimon, index) => {
+                  if (showMore) {
+                    if (level === "Todos") {
+                      return (
                         <DigimonCard
                           key={digimon.name}
                           name={digimon.name}
                           imgUrl={digimon.img}
                           level={digimon.level}
                         />
-                      </>
-                    );
-                  }
-                }
-              }
-            })}
-          </ul>
-          {!showMore && filteredDigimons.length > 3 && (
-            <button
-              onClick={() => setShowMore(!showMore)}
-              className="bg-blue-900 text-white px-4 py-2 rounded-md "
-            >
-              Mostrar Mais
-            </button>
-          )}
+                      );
+                    }
 
-          {showMore && filteredDigimons.length > 3 && (
-            <button
-              onClick={() => setShowMore(!showMore)}
-              className="bg-red-900 text-white px-4 py-2 rounded-md fixed -translate-x-1/2 left-1/2 bottom-10"
-            >
-              Mostrar Menos
-            </button>
+                    if (digimon.level === level) {
+                      return (
+                        <DigimonCard
+                          key={digimon.name}
+                          name={digimon.name}
+                          imgUrl={digimon.img}
+                          level={digimon.level}
+                        />
+                      );
+                    }
+                  } else {
+                    if (index < 3) {
+                      if (level === "Todos") {
+                        return (
+                          <DigimonCard
+                            key={digimon.name}
+                            name={digimon.name}
+                            imgUrl={digimon.img}
+                            level={digimon.level}
+                          />
+                        );
+                      }
+
+                      if (digimon.level === level) {
+                        return (
+                          <>
+                            <DigimonCard
+                              key={digimon.name}
+                              name={digimon.name}
+                              imgUrl={digimon.img}
+                              level={digimon.level}
+                            />
+                          </>
+                        );
+                      }
+                    }
+                  }
+                })}
+              </ul>
+              {!showMore && filteredDigimons.length > 3 && (
+                <button
+                  onClick={() => setShowMore(!showMore)}
+                  className="bg-blue-900 text-white px-4 py-2 rounded-md "
+                >
+                  Mostrar Mais
+                </button>
+              )}
+
+              {showMore && filteredDigimons.length > 3 && (
+                <button
+                  onClick={() => setShowMore(!showMore)}
+                  className="bg-red-900 text-white px-4 py-2 rounded-md fixed -translate-x-1/2 left-1/2 bottom-10"
+                >
+                  Mostrar Menos
+                </button>
+              )}
+            </>
           )}
         </>
       )}
